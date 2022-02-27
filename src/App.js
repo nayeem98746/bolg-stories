93596fb45a06331c45f1506a3ebce71e7642ab77
nayeem407 <sabbirahmednayeem941@gmail.com>
2022-02-27 17:28:08 +0600

    login page commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import * as React from "react";
 import Home from './components/Home/Home';
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import './Login.css'
+
 
 const Login = () => {
+    const [loginFromData ,  setLoginFromData] = useState({})
+
+    const handleLoginSubmit = e => {
+        e.preventDefault()
+        console.log();
+      
+    }
+
+
     return (
-        <div>
-            <h2>this is login</h2>
+      <div className='main-section'>
+            <div className='container'>
+            <header className='header-font'>Login Form</header>
+            <form  className='form-intro' action="" onSubmit={handleLoginSubmit} >
+                <div className='input-field'>
+                    <input
+                     className='input-style' type="email" 
+                     name='email'
+                     label='email'
+                    //  onBlur={handleOnBlur}  
+                     required 
+                     
+                     />
+                    <label htmlFor="">Email or Username</label>
+                </div>
+                <div className='input-field'>
+                    <input className='input-style'
+                      type="password" 
+                      required 
+                      name='password'
+                    //   onBlur={handleOnBlur}  
+                    label='password'
+                      
+                      />
+                    <label htmlFor="">Password</label>
+                </div>
+                <div className='button'>
+                    <div className='inner'></div>
+                    <button className='from-button'  
+                    type='submit'
+                    >Login</button>
+                    <NavLink style={{marginTop:'5px'}} as={Link} to='/register' >Please Register</NavLink>
+                </div>
+            </form>
         </div>
+      </div>
     );
 };
 
